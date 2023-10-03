ffb2bff736c873a99807725571e10126456008e6
AyeshanAnuradha <ayeshan98udara@gmail.com>
2023-10-03 12:01:39 +0530

    UpdateQA

--- phpQA/QA5/index.php
+++ phpQA/QA5/index.php
@@ -1,30 +1,11 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form method="GET">
 
-    <input type="text" name="name1">FirstWord</input>
-</br>
-
-    <input type="text" name="name2">SecondWord</input>
-
-</br>
-    <input type="submit"></input>
-
-</br>
-    </form>
 
     <?php 
-    $var1 = $_GET["name1"];
-    $var2 = $_GET["name2"];
+    $var1 = "Udara " ;
+    $var2 = "Ayeshan";
 
-    $var3 = $var1.$var2;
+    $var3 = $var1 . $var2;
 
     echo $var3;
 
@@ -32,5 +13,3 @@
     
     ?>
     
-</body>
-</html>
