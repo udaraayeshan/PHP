7281c97832f9324c470a2ba7f67a929734e1e8d7
AyeshanAnuradha <ayeshan98udara@gmail.com>
2023-10-03 15:24:22 +0530

    Predetermined Function

---
+++ 5.phpfunctions/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        //  Predetermined Function
+       
+       echo strlen("udaraayeshan")."<br>";
+
+       echo strrev("udara")."<br>";
+       echo str_word_count("udara ayeshan")."<br>";
+       echo strpos("CMB is youtube channel"  ,"y");
+
+       
+    ?>
+</body>
+</html>
