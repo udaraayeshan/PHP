e81d1bf1ec1144d12165794566aa16107eeea263
AyeshanAnuradha <ayeshan98udara@gmail.com>
2023-10-06 13:18:20 +0530

    phpConditionalStatements

---
+++ 8.phpConditionalStatement/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    $x = 100;
+
+      if($x == 10) {
+         echo "Out put is 10" ;
+      }elseif($x == 20) {
+         echo "Out put is 20" ;
+      }elseif($x == 30) {
+         echo "Out put is 30" ;
+      }elseif($x == 40) {
+         echo "Out put is 40" ;
+      }elseif($x == 50) {
+         echo "Out put is 50" ;
+      }else{
+        echo "Wrong";
+      }
+    
+    ?>
+
+</body>
+</html>
