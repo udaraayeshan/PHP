924212c2ae6353d55a3e14268733f1e9aec08c32
AyeshanAnuradha <ayeshan98udara@gmail.com>
2023-10-06 13:53:17 +0530

    phpswitch

---
+++ 9.phpSwitch/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+    
+    $x = 100;
+
+    switch ($x) {
+       case 5:
+         echo "Input Value is 5";
+          break;
+       case 10:
+         echo "Input Value is 10";
+             break;
+       case 15:
+         echo "Input Value is 15";
+                 break;
+        default:
+         echo "wrong";
+          break;
+
+
+    }
+
+
+    //   if($x == 10) {
+    //      echo "Out put is 10" ;
+    //   }elseif($x == 20) {
+    //      echo "Out put is 20" ;
+    //   }elseif($x == 30) {
+    //      echo "Out put is 30" ;
+    //   }elseif($x == 40) {
+    //      echo "Out put is 40" ;
+    //   }elseif($x == 50) {
+    //      echo "Out put is 50" ;
+    //   }else{
+    //     echo "Wrong";
+    //   }
+    
+    ?>
+</body>
+</html>
