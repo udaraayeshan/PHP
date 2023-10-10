f70b1e1b3f76c8fcb2a6bfa23d20f0092563527c
AyeshanAnuradha <ayeshan98udara@gmail.com>
2023-10-10 12:24:53 +0530

    phpIncludeDocument

---
+++ 11.phpIncludeDocument/new.php
@@ -0,0 +1,25 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+
+        function familyName($fname, $year) {
+                    echo "$fname Refsnes. Born in $year <br>";
+                  }
+                  
+                  familyName("Hege", "1970");
+                  familyName("Stale", "1970");
+                  familyName("Kai Jim", "1980");
+
+    ?>
+
+</body>
+</html>
