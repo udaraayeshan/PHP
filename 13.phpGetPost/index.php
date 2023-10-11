ad8b09e5915f7e42b969b4749ae8b92fbdcd0728
AyeshanAnuradha <ayeshan98udara@gmail.com>
2023-10-11 15:13:47 +0530

    phpGetPost

---
+++ 13.phpGetPost/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+                         
+<?php 
+
+         // GET / POST
+              
+
+echo $_POST['password']
+
+
+?>
+
+
+
+<form method="POST">
+    <input type="hidden" name="name" value="DTK"> </br></br>
+    <input type="email" name="userInput"> </br></br>
+    <input type="password" name="password"> </br></br>
+    <input type="submit" value="Click Here"> 
+</form>
+
+</body>
+</html>
